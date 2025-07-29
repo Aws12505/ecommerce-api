f908f92245fb1d6c5846b904bb5102e1decafa55
Aws12505 <awsaljaramani12505@gmail.com>
2025-07-29 09:33:08 -0400

    .

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -13,13 +13,6 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusherapp.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => true,
-            ],
-            'client_options' => [
-                'verify_peer' => env('PUSHER_VERIFY_PEER', true),
             ],
         ],
 
